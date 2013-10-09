f3b4190d6eba2e3ab0e14d38ea40fa6cbfc3a8c1
yangliu <leonefly@hotmail.com>
2013-10-09 14:37:28 -0400

    Yangliu1

---
+++ src/Ondemand.java
@@ -0,0 +1,505 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.io.Writer;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TimeZone;
+
+import ch.ethz.ssh2.Connection;
+import ch.ethz.ssh2.Session;
+import ch.ethz.ssh2.StreamGobbler;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClient;
+import com.amazonaws.services.cloudwatch.model.Datapoint;
+import com.amazonaws.services.cloudwatch.model.Dimension;
+import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsRequest;
+import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsResult;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.AllocateAddressResult;
+import com.amazonaws.services.ec2.model.AssociateAddressRequest;
+import com.amazonaws.services.ec2.model.AuthorizeSecurityGroupIngressRequest;
+import com.amazonaws.services.ec2.model.CreateKeyPairRequest;
+import com.amazonaws.services.ec2.model.CreateKeyPairResult;
+import com.amazonaws.services.ec2.model.CreateSecurityGroupRequest;
+import com.amazonaws.services.ec2.model.CreateTagsRequest;
+import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
+import com.amazonaws.services.ec2.model.DescribeImagesResult;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.DescribeKeyPairsResult;
+import com.amazonaws.services.ec2.model.DisassociateAddressRequest;
+import com.amazonaws.services.ec2.model.Image;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.InstanceState;
+import com.amazonaws.services.ec2.model.IpPermission;
+import com.amazonaws.services.ec2.model.KeyPair;
+import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
+import com.amazonaws.services.ec2.model.StartInstancesRequest;
+import com.amazonaws.services.ec2.model.StopInstancesRequest;
+import com.amazonaws.services.ec2.model.Tag;
+import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+
+public class Ondemand {
+	
+	//Time to get statistics from cloud watch
+	public static int cloudwatchtime = 60;
+	//Maximum days for a VM to exist
+	public static int maxDays = 2;
+	//Night and day duration in seconds
+	public static int nightTime = 5*60;
+	public static int dayTime = 4*60;
+	//Threshold of a computer being idle
+	public static double cpuIdle = 0.0;
+	/*
+	 * private key storage path
+	 */
+	public static String keyPath = "";
+	
+	//Upper/lower threshold
+	public static double upperTh = 50.0;
+	public static double lowerTh = 20.0;
+	
+	 static KeyPair privateKey;
+	 static int groupnum = 15;
+	
+	public static void main(String[] args) throws Exception{
+		
+		AWSCredentials credentials = new PropertiesCredentials(
+   			 Ondemand.class.getResourceAsStream("AwsCredentials.properties"));
+		
+        AmazonEC2 ec2 = new AmazonEC2Client(credentials);
+        AmazonS3Client s3 = new AmazonS3Client(credentials);
+        AmazonCloudWatchClient cloudWatch = new AmazonCloudWatchClient(credentials);
+           String counter = "1";
+    	   String securitygroup = "assigment" + counter;
+    	   String keypair = "workkey" + counter;
+    	   String zone = "us-east-1a";
+    	   String bucketname = "assign1bucket" + counter;
+    	   String imageID = "ami-76f0061f";
+    	   
+    	   createSecurityGroup(ec2, securitygroup);
+    	   createKey(ec2, keypair);
+    	   createS3Bucket(s3, bucketname,zone);
+    	   createS3File(s3,bucketname);
+    	   
+    	   OndemandEC2 worker1 = new OndemandEC2(securitygroup,keypair,imageID,zone,"worker1");
+    	   OndemandEC2 worker2 = new OndemandEC2(securitygroup,keypair,imageID,zone,"worker2");
+           
+    	   //For auto scaling
+    	   OndemandEC2 worker3 = new OndemandEC2(securitygroup,keypair,imageID, zone, "worker3");
+    	   
+    	   List<OndemandEC2> machines = Arrays.asList(worker1,worker2);
+    	   
+    	   int days = 1;
+    	   int count = 0;
+    	   
+    	   while (true){
+    		   
+    		   if (startTime(count)){
+    			   count++;
+    			   System.out.println("Daytime begins. This is : DAY "+days);
+    			   
+    			   //create the instances for each user
+    			   for(OndemandEC2 machine : machines)
+    				   createMachine(machine, bucketname);
+    			   
+    			   System.out.println("Now all machines are created.\r\nPlease wait for initialization.");
+    			   
+    			   if (days <=1){
+    				   Thread.sleep(2*60*1000);
+    				   System.out.println("Start working now.");
+    				   increaseCPU(worker1, keypair);  //ssh initialization, start working
+    				   increaseCPU(worker2, keypair);
+    			   }		   
+    			   
+    			   sleep(cloudwatchtime); 
+    			   
+    			   continue;  
+    		   }
+    		   else if (endTime(count)){
+    			   count = 0;
+    			   days++;
+    			   
+    			   //Time to end, terminate all machines
+    			   
+    			   for (OndemandEC2 machine : machines){
+    				   closeMachine(machine);
+    			   }
+    			   //Worker for auto-scale, not included in the list
+    			   if (!worker3.getIsTerminated(false))
+    				   worker3.shutDown();
+    			   
+    			   if (days>maxDays){
+    				   System.out.println("Reach the maximum day of working!");
+    				   break;
+    			   }
+    			   System.out.println("End of Daytime. All machines are terminiated.\r\nNighttime begins.");
+    			   sleep(nightTime);
+    			   continue;
+    		   }
+    		   
+    		   count++;
+    		   
+    		   if ((days == 1)&&(count>3)){
+    			   if (worker1.getIsTerminated(false))
+    			   releaseCPU(worker1, keypair);
+    			   if (worker2.getIsTerminated(false))
+    			   releaseCPU(worker2, keypair);
+    		   }
+    		   
+    		   //Terminate idle machines
+    		   for(OndemandEC2 machine : machines){
+    			   if(isIdle(cloudWatch, machine, cpuIdle)){
+    				   
+    				   System.out.println(machine.machinename + " is idle, terminate it.");  
+    				   closeMachine(machine);
+    				     				   
+    			   }   
+    		   }
+    		   //Auto-scale
+    		   autoScale(worker3, cpuUtil(cloudWatch, worker2.instanceID));
+    		   
+    		   System.out.println("Hours running: " + count);
+    		   
+    		   sleep(cloudwatchtime);
+    	   }
+    	   
+    	   ec2.shutdown();
+    	   s3.shutdown();
+    	   cloudWatch.shutdown();
+	}
+	
+	
+	public static void createSecurityGroup(AmazonEC2 ec2, String groupname){
+		
+		CreateSecurityGroupRequest createSecurityGroupRequest = new CreateSecurityGroupRequest();
+        createSecurityGroupRequest.withGroupName(groupname).withDescription("Assignment1");
+        ec2.createSecurityGroup(createSecurityGroupRequest);
+        AuthorizeSecurityGroupIngressRequest authorizeSecurityGroupIngressRequest = new AuthorizeSecurityGroupIngressRequest();
+        authorizeSecurityGroupIngressRequest.setGroupName(groupname);
+        
+        /**********
+         * IP permissions 
+         * **********/
+        //http
+        IpPermission http = new IpPermission();
+        http.setIpProtocol("tcp");
+        http.setFromPort(80);
+        http.setToPort(80);
+        List<String> ipRangeshttp = new ArrayList<String>();
+        ipRangeshttp.add("0.0.0.0/0");
+        http.setIpRanges(ipRangeshttp);
+        
+        //ssh
+        IpPermission ssh = new IpPermission();
+        ssh.setIpProtocol("tcp");
+        ssh.setFromPort(22);
+        ssh.setToPort(22);
+        List<String> ipRangesssh = new ArrayList<String>();
+        ipRangesssh.add("0.0.0.0/0");
+        ssh.setIpRanges(ipRangesssh);
+        
+        //tcp 
+        IpPermission tcp  = new IpPermission();
+        tcp.setIpProtocol("tcp");
+        tcp.setFromPort(0);
+        tcp.setToPort(65535);
+        List<String> ipRangestcp = new ArrayList<String>();
+        ipRangestcp.add("0.0.0.0/0"); 
+        tcp.setIpRanges(ipRangestcp);
+        
+        List<IpPermission> permissions = new ArrayList<IpPermission>();
+        permissions.add(http);
+        permissions.add(ssh);
+        permissions.add(tcp);
+        authorizeSecurityGroupIngressRequest.setIpPermissions(permissions);
+        
+        ec2.authorizeSecurityGroupIngress(authorizeSecurityGroupIngressRequest);
+        List<String> groupNames = new ArrayList<String>();
+        groupNames.add(groupname);
+        System.out.println("Security Group created successfully: " + groupname);
+	}
+	
+	public static void createKey(AmazonEC2 ec2, String keypair) throws IOException{
+		 
+        CreateKeyPairRequest newKeyRequest = new CreateKeyPairRequest();
+        newKeyRequest.setKeyName(keypair);
+        CreateKeyPairResult keyresult = ec2.createKeyPair(newKeyRequest);
+        
+        privateKey = keyresult.getKeyPair();
+      
+        String fileName=keypair+".pem"; 
+        File distFile = new File(fileName); 
+        BufferedReader bufferedReader = new BufferedReader(new StringReader(privateKey.getKeyMaterial()));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(distFile)); 
+        char buf[] = new char[1024];        
+        int len; 
+        while ((len = bufferedReader.read(buf)) != -1) { 
+                bufferedWriter.write(buf, 0, len); 
+        } 
+        bufferedWriter.flush(); 
+        bufferedReader.close(); 
+        bufferedWriter.close(); 
+        
+        System.out.println("Private key created successfully: " + keypair);
+	}
+	
+	public static void createS3Bucket(AmazonS3Client s3, String bucketname, String zone){
+		List <Bucket> bucketnames = s3.listBuckets();
+		
+		for (Bucket bucket : bucketnames){
+			if (bucketname.equalsIgnoreCase(bucket.getName())){
+				System.out.println("Bucket already exists");
+				return;
+			}
+		}
+		s3.createBucket(bucketname);
+		System.out.println("Bucket created successfully: " + bucketname);
+		
+		return;
+		
+	}
+	
+	public static void createS3File(AmazonS3Client s3, String bucketname) throws IOException{
+		
+        //set key
+        String key = "object-name.txt";
+        
+        //set value
+        File file = File.createTempFile("temp", ".txt");
+        //file.deleteOnExit();
+        Writer writer = new OutputStreamWriter(new FileOutputStream(file));
+        writer.write("This is a sample sentence.");
+        writer.close();
+        
+        //put object - bucket, key, value(file)
+        s3.putObject(new PutObjectRequest(bucketname, key, file));
+        System.out.println("A file "+ key + " is put in to the bucket: " +bucketname);
+	}
+	
+	private static Boolean startTime(int count){
+		return count == 0;
+	}
+	
+	private static Boolean endTime(int count){
+		return count*cloudwatchtime > dayTime;
+	}
+	
+	private static void createMachine(OndemandEC2 machine, String bucketname) throws IOException{
+		
+		System.out.println("Instance is creating...");
+		machine.createInstane();
+		
+		sleep(10);  //Sleep before starting up
+		
+		machine.startUp();
+		System.out.println("Attach EBS volume " +machine.volumeID +" to the instance " +machine.instanceID);
+		machine.attachVolume();
+		System.out.println("Attach S3 " +bucketname +" to the instance " +machine.instanceID);
+		machine.attachS3(bucketname);
+		
+		
+	}
+	
+	private static void closeMachine(OndemandEC2 machine){
+		
+		if(machine.getIsTerminated(true)){
+			return;
+		}
+		
+		System.out.println("Detach the EBS volume " +machine.volumeID);
+		machine.detachVolume();
+		System.out.println("Save Snapshot");
+		machine.saveSnapShot();
+		System.out.println("Terminate the machine");
+		machine.shutDown();
+
+		
+	}
+	
+	static void increaseCPU(OndemandEC2 worker, String keypair) throws InterruptedException {
+		
+		File file = new File(keypair + ".pem");
+		//String filePass = "none";
+		
+		try{
+			Connection connection = new Connection(worker.ipAddress);
+			connection.connect();
+			
+			boolean auth = connection.authenticateWithPublicKey("ec2-user", file, "none");
+			if (auth == false)
+				throw new IOException("Authentification failed");
+			
+			Session session = connection.openSession();
+			System.out.println("Increase CPU of "+worker.machinename);
+			session.execCommand("while true; do true; done");
+			session.close();
+			connection.close();
+			
+			//System.out.println("Increase CPU of "+worker.machinename);
+		}
+		catch (IOException e){
+			e.printStackTrace(System.err);
+			System.out.println("Please use the attached script to start and stop cpu remotely");
+		}
+	}
+	
+	static void  releaseCPU(OndemandEC2 worker, String keypair) throws InterruptedException{
+		File file = new File(keypair + ".pem");
+		//String filePass = "none";
+		
+		try{
+			Connection connection = new Connection(worker.ipAddress);
+			connection.connect();
+			
+			boolean auth = connection.authenticateWithPublicKey("ec2-user", file, "none");
+			if (auth == false)
+				throw new IOException("Authentification failed");
+			
+			Session session = connection.openSession();
+			session.execCommand("killall bash");
+			session.close();
+			connection.close();
+			System.out.println("Release CPU of "+ worker.machinename);
+		}
+		catch (IOException e){
+			e.printStackTrace(System.err);
+			System.out.println("Please use the attached script to start and stop cpu remotely");
+		}
+	}
+	
+	public static void sleep(int sec){
+		try{
+			Thread.sleep(sec*1000);
+		}
+		catch(InterruptedException e){
+			System.out.println("Caught Exception: " + e.getMessage());
+		}
+	}
+	
+	private static Boolean isIdle(AmazonCloudWatchClient cloudWatch, OndemandEC2 machine, double cpuIdle){
+		
+		if (machine.getIsTerminated(true))
+			return false;
+		double utilization = cpuUtil(cloudWatch, machine.instanceID);
+		if (utilization < 0) 
+			return false;
+		return utilization <= cpuIdle;  
+	}
+
+	public static double cpuUtil(AmazonCloudWatchClient cloudWatch, String instanceID){
+
+		try{
+			
+			//create request message
+			GetMetricStatisticsRequest statRequest = new GetMetricStatisticsRequest();
+			
+			//set up request message
+			statRequest.setNamespace("AWS/EC2"); //namespace
+			statRequest.setPeriod(60); //period of data
+			ArrayList<String> stats = new ArrayList<String>();
+			
+			//Use one of these strings: Average, Maximum, Minimum, SampleCount, Sum 
+			stats.add("Average"); 
+			stats.add("Sum");
+			statRequest.setStatistics(stats);
+			
+			//Use one of these strings: CPUUtilization, NetworkIn, NetworkOut, DiskReadBytes, DiskWriteBytes, DiskReadOperations  
+			statRequest.setMetricName("CPUUtilization"); 
+			
+			// set time
+			GregorianCalendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+			calendar.add(GregorianCalendar.SECOND, -1 * calendar.get(GregorianCalendar.SECOND)); // 1 second ago
+			Date endTime = calendar.getTime();
+			calendar.add(GregorianCalendar.MINUTE, -1); // 1 minutes ago
+			Date startTime = calendar.getTime();
+			statRequest.setStartTime(startTime);
+			statRequest.setEndTime(endTime);
+			
+			//specify an instance
+			ArrayList<Dimension> dimensions = new ArrayList<Dimension>();
+			dimensions.add(new Dimension().withName("InstanceID").withValue(instanceID));
+			statRequest.setDimensions(dimensions);
+			//System.out.println(dimensions);
+			
+			//get statistics
+			GetMetricStatisticsResult statResult = cloudWatch.getMetricStatistics(statRequest);
+			
+			//display
+			//System.out.println(statResult.toString());
+			List<Datapoint> dataList = statResult.getDatapoints();
+			Double averageCPU = null;
+			Date timeStamp = null;
+			for (Datapoint data : dataList){
+				averageCPU = data.getAverage();
+				timeStamp = data.getTimestamp();
+				//System.out.println("Average CPU utililization for last 10 minutes: "+averageCPU);
+				//System.out.println("Total CPU utililization for last 10 minutes: "+data.getSum());
+			}
+            
+            if (averageCPU == null) {
+            	System.out.println(instanceID + " : average CPU utlilization for last hour is 0");
+            	return 0;
+            }
+            else{
+            	System.out.println(instanceID + " : average CPU utlilization for last hour is "+averageCPU);
+            	return averageCPU;
+            }
+            
+		} catch (AmazonServiceException ase) {
+		    System.out.println("Caught Exception: " + ase.getMessage());
+		    System.out.println("Reponse Status Code: " + ase.getStatusCode());
+		    System.out.println("Error Code: " + ase.getErrorCode());
+		    System.out.println("Request ID: " + ase.getRequestId());
+		    return 0;
+		}
+	}
+	
+	public static void autoScale(OndemandEC2 worker3, double value){
+		
+		Boolean terminate = worker3.getIsTerminated(false);
+		
+		if (worker3.getIsTerminated(false)&& value>upperTh){
+			System.out.println("CPU utilization exceeds threshold, create one more machine.");
+			worker3.createInstane();		
+		}
+		else if (!terminate && value<lowerTh){
+			System.out.println("CPU utilization below threshold, close one machine.");
+			worker3.shutDown();
+		}
+	}
+}
+	
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/OndemandEC2.java
@@ -0,0 +1,261 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.AllocateAddressResult;
+import com.amazonaws.services.ec2.model.AssociateAddressRequest;
+import com.amazonaws.services.ec2.model.AttachVolumeRequest;
+import com.amazonaws.services.ec2.model.CreateImageRequest;
+import com.amazonaws.services.ec2.model.CreateImageResult;
+import com.amazonaws.services.ec2.model.CreateTagsRequest;
+import com.amazonaws.services.ec2.model.CreateVolumeRequest;
+import com.amazonaws.services.ec2.model.CreateVolumeResult;
+import com.amazonaws.services.ec2.model.DescribeImagesRequest;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.DetachVolumeRequest;
+import com.amazonaws.services.ec2.model.DisassociateAddressRequest;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.InstanceState;
+import com.amazonaws.services.ec2.model.Placement;
+import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
+import com.amazonaws.services.ec2.model.StartInstancesRequest;
+import com.amazonaws.services.ec2.model.Tag;
+import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
+import com.amazonaws.services.opsworks.model.StartInstanceRequest;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+
+
+public class OndemandEC2 {
+	AmazonEC2 ec2;
+	AmazonS3Client s3;
+	AWSCredentials credentials;
+	String instanceID;
+	String keyname;
+	String groupname;
+	String machinename;
+	String zone;
+	String imageID;
+	String volumeID;
+	String ipAddress;
+	Boolean isTerminated = true;
+	
+    public OndemandEC2(String securitygroup, String keypair, String imageid,String zone1, String computername) throws IOException, InterruptedException{
+    	credentials = new PropertiesCredentials(
+				OndemandEC2.class.getResourceAsStream("AwsCredentials.properties"));
+		
+		ec2 = new AmazonEC2Client(credentials);
+		s3  = new AmazonS3Client(credentials);		
+		
+		groupname = securitygroup;
+		keyname = keypair;
+		machinename = computername;
+		zone = zone1;
+		imageID = imageid;
+		this.createVolume(10);
+    	}
+    
+    public void createInstane(){
+    	
+		Placement place = new Placement();
+		place.setAvailabilityZone(zone);
+		
+    	int minInstanceCount = 1; 
+        int maxInstanceCount = 1;
+        RunInstancesRequest rir = new RunInstancesRequest(imageID, minInstanceCount, maxInstanceCount);
+        
+        rir.setMonitoring(true); //open monitering
+        rir.setPlacement(place);
+        rir.setInstanceType("t1.micro");
+        rir.setKeyName(keyname);// set key pair
+        
+        List<String> securitygroups = new ArrayList<String>();
+        securitygroups.add(groupname);
+        rir.setSecurityGroups(securitygroups);// set security group
+        RunInstancesResult result = ec2.runInstances(rir);
+        
+        //get instanceId from the result
+        List<Instance> resultInstance = result.getReservation().getInstances();
+        InstanceState state = null;
+        
+        int instancenum = resultInstance.size();
+        Instance ins = resultInstance.get(instancenum-1);
+        instanceID = ins.getInstanceId();
+        state = ins.getState();
+        
+        System.out.println("The instance ID is " +instanceID);
+        
+        List<String> resources = new LinkedList<String>();
+        List<Tag> tags = new LinkedList<Tag>();
+        Tag nameTag = new Tag("Name", "Assignment1");
+        
+        resources.add(instanceID);
+        tags.add(nameTag);
+        
+        CreateTagsRequest ctr = new CreateTagsRequest(resources, tags);
+        ec2.createTags(ctr);
+        
+        //Wait for the new machine to run
+        try{
+        		
+        		Thread.sleep(40000);
+        	}
+        	catch (InterruptedException e){
+        		e.printStackTrace();
+        	}
+        state = ins.getState();
+		//System.out.println(state.getName());
+        
+        if (this.ipAddress == null){
+        	this.createElasticIp();
+        }
+        //System.out.println("Machine " + machinename +"created ip");
+        this.assignElasticIp();
+        
+        isTerminated = false;
+        System.out.println("Instance created: " + machinename + " with ID = " + this.instanceID + " IP = " + this.ipAddress + " and AMI = " + this.imageID);
+    }
+    
+    public Instance getInstance(){
+    	
+        DescribeInstancesResult describeInstancesRequest = ec2.describeInstances();
+        List<Reservation> reservations = describeInstancesRequest.getReservations();
+        Set<Instance> instances = new HashSet<Instance>();
+        // add all instances to a Set.
+        for (Reservation reservation : reservations) {
+        	instances.addAll(reservation.getInstances());
+        }
+        
+                
+        for ( Instance ins : instances){
+           	if ( instanceID.equalsIgnoreCase(ins.getInstanceId()) == true ){
+        		return ins;
+        	}
+        }
+        
+        return null;
+        
+    }
+    
+    public void createElasticIp(){
+    	AllocateAddressResult elasticResult = ec2.allocateAddress();
+		ipAddress = elasticResult.getPublicIp();	
+    }
+    
+    public void assignElasticIp(){ 	
+    	AssociateAddressRequest aar = new AssociateAddressRequest();
+		aar.setInstanceId(instanceID);
+		aar.setPublicIp(ipAddress);
+		ec2.associateAddress(aar);   	
+    }
+    
+    public void disassociateElasticIp(){	
+		DisassociateAddressRequest dar = new DisassociateAddressRequest();
+		dar.setPublicIp(this.ipAddress);
+		ec2.disassociateAddress(dar);
+	}
+    
+    public void  shutDown(){
+    	List<String> tmp = new ArrayList<String>();
+    	tmp.add(instanceID);
+    	TerminateInstancesRequest tir = new TerminateInstancesRequest(tmp);
+        ec2.terminateInstances(tir);   	
+    }
+    
+    public void startUp(){
+    	List<String> tmp = new ArrayList<String>();
+    	tmp.add(instanceID);
+    	StartInstancesRequest sir = new StartInstancesRequest(tmp);
+    	ec2.startInstances(sir);
+    }
+    
+    public void createVolume(int size){
+    	CreateVolumeRequest cvr = new CreateVolumeRequest();
+        cvr.setAvailabilityZone(this.zone);
+        cvr.setSize(size);
+    	CreateVolumeResult volumeResult = ec2.createVolume(cvr);
+    	String createdVolumeId = volumeResult.getVolume().getVolumeId();
+    	this.volumeID = createdVolumeId;
+    }
+    
+    public void attachVolume(){
+    	AttachVolumeRequest avr = new AttachVolumeRequest();
+    	//System.out.println(this.volumeID);
+    	avr.setVolumeId(this.volumeID);
+    	avr.setInstanceId(this.instanceID);
+    	avr.setDevice("/dev/sdf");
+    	ec2.attachVolume(avr);
+    }
+    
+    public void detachVolume(){
+    	DetachVolumeRequest dvr = new DetachVolumeRequest();
+    	dvr.setVolumeId(this.volumeID);
+    	dvr.setInstanceId(this.instanceID);
+    	ec2.detachVolume(dvr);
+    }
+    
+    
+   	public void attachS3(String bucketName) throws IOException {
+           //set key
+           String key = "object-name.txt";
+                   
+           //get object
+           S3Object object = s3.getObject(new GetObjectRequest(bucketName, key));
+           BufferedReader reader = new BufferedReader(
+           	    new InputStreamReader(object.getObjectContent()));
+           String dataS3 = null;
+           System.out.print(this.machinename + ": ");
+           while ((dataS3 = reader.readLine()) != null) {
+               System.out.println(dataS3);
+           }
+   	}
+   	
+    public String saveSnapShot() {
+	
+	    CreateImageRequest cir = new CreateImageRequest();
+	    cir.setInstanceId(this.instanceID);
+	    cir.setName(this.instanceID + "__AMI"); //we can choose the name
+	    
+	    CreateImageResult createImageResult = ec2.createImage(cir);
+	    String createdImageId = createImageResult.getImageId();
+	    
+	    System.out.println("AMI is created. AMI id=" + createdImageId);
+	    
+	    //Save the AMI
+	    this.imageID = createdImageId;
+	    
+	    return createdImageId;
+    }
+
+    //Should be invoked after the saveSnapShot has been called
+    public String getSnapShotState() {
+		DescribeImagesRequest dir = new DescribeImagesRequest();
+		dir.setImageIds(Arrays.asList(this.imageID));
+		String state = ec2.describeImages(dir).getImages().get(0).getState();
+		
+		return state;
+    }
+
+    //Returns a bool value that indicates whether the machine is terminated
+    public Boolean getIsTerminated(Boolean getFromService) {
+    	if (!getFromService) return this.isTerminated;
+	
+    	Instance ins = this.getInstance();
+    	if (ins == null || ins.getState().getCode() > 16) 
+    		return true;
+    	return false;
+    }
+}
